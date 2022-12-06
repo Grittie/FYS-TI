67ce1a587f1ed91836801d1a96cca6698e394d26
l.hilgevoord <lucashilgevoord@hotmail.com>
2022-12-06 23:42:39 +0100

    Created segment testing logic with sevSeg lib

--- testing/segment_display/nano_segment_display/src/main.cpp
+++ testing/segment_display/nano_segment_display/src/main.cpp
@@ -1,118 +1,38 @@
-#include <Arduino.h>
+#include "SevSeg.h"
+SevSeg sevseg;
 
-/*
-  Showing number 0-9 on a Common Anode 7-segment LED display
-      A(9)
-      ---
-F(8) |   | B(2)
-     |   |
-      --- G(7)
-E(6) |   | C(3)
-     |   |
-      ---     .(4)
-      D(5)
+int countdown = 0;
+int max_countdown = 5000;
 
-D1: 10
-D2: 11
-D3: 12
-D4: 13
- */
-
-const byte digitalPins[] = {2, 3, /*4,*/ 5, 6, 7, 8, 9};
-int digitalPinLenght = 0;
-
-const byte segmentPins[] = {10, 11, 12, 13};
-int segmentPinLenght = 0;
-
-const int buttonPin = 4;
-int buttonState = 0;
-
-int loopIndex = 0;
-bool isPressed = false;
-
-int digitalNumbers[10][8] = {
-    {1, 1, /*1,*/ 1, 1, 0, 1, 1}, // 0
-    {0, 0, /*0,*/ 0, 1, 0, 1, 0}, // 1
-    {1, 0, /*0,*/ 1, 1, 1, 0, 1}, // 2
-    {1, 1, /*0,*/ 1, 0, 1, 0, 1}, // 3
-    {1, 1, /*0,*/ 0, 0, 1, 1, 0}, // 4
-    {0, 1, /*0,*/ 1, 0, 1, 1, 1}, // 5
-    {0, 1, /*0,*/ 1, 1, 1, 1, 1}, // 6
-    {1, 1, /*0,*/ 0, 0, 0, 0, 1}, // 7
-    {1, 1, /*0,*/ 1, 1, 1, 1, 1}, // 8
-    {1, 1, /*0,*/ 1, 0, 1, 1, 1}, // 9
-};
-int dititalNumberLength = 0;
+int number = 0;
 char buffer[5];
 
-// the setup routine runs once when you press reset:
-void setup()
-{
-  Serial.begin(9600);
-  Serial.print("Setup \n");
-  digitalPinLenght = sizeof(digitalPins) / sizeof(digitalPins[0]);
-  segmentPinLenght = sizeof(segmentPins) / sizeof(segmentPins[0]);
-  dititalNumberLength = sizeof(digitalNumbers) / sizeof(digitalNumbers[0]);
+void setup() {
+    Serial.begin(9600);
 
-  // initialize the digital pins
-  for (size_t i = 0; i < digitalPinLenght; i++)
-  {
-    pinMode(digitalPins[i], OUTPUT);
-    digitalWrite(digitalPins[i], digitalNumbers[0][i]);
-  }
-
-  // initialize the digital pins
-  for (size_t i = 0; i < segmentPinLenght; i++)
-  {
-    pinMode(segmentPins[i], OUTPUT);
-  }
-
-  pinMode(buttonPin, INPUT);
+    byte numDigits = 4;  
+    byte digitPins[] = {2, 3, 4, 5};
+    byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+    bool resistorsOnSegments = 0; 
+    sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins, resistorsOnSegments);
+    sevseg.setBrightness(90);
 }
 
-void setDitgitalNumber(int number)
-{
-  sprintf(buffer, "%04d", number);
-  Serial.println(buffer);
-
-  for (size_t i = 0; i < segmentPinLenght; i++)
-  {
-    digitalWrite(segmentPins[i], HIGH);
-    for (size_t j = 0; j < segmentPinLenght; j++)
+void loop() {
+    sprintf(buffer, "%04d", number);
+    char numb;
+    for (size_t i = 0; i < 4; i++)
     {
-      if (j != i)
-      {
-        digitalWrite(segmentPins[j], LOW);
-      }
+      numb = numb + buffer[i];
+    }
+    countdown++;
+
+    if (countdown >= max_countdown) {
+      number++;
+      countdown = 0;
     }
 
-    for (size_t j = 0; j < dititalNumberLength; j++)
-    {
-      int n = buffer[i] - '0';
-      digitalWrite(digitalPins[j], digitalNumbers[loopIndex][j]);
-    }
-  }
+    sevseg.setChars(buffer);
+    sevseg.refreshDisplay(); // Must run repeatedly
 }
 
-void loop()
-{
-  buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH)
-  {
-    // turn on:
-    if (!isPressed)
-    {
-      Serial.println("pressed?");
-      setDitgitalNumber(loopIndex);
-      loopIndex++;
-      isPressed = true;
-    }
-  }
-  else
-  {
-    // turn off:
-    isPressed = false;
-  }
-}
