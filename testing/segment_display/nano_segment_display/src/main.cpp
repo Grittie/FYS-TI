f9d5490c561babfd3851aece815296b84be56456
l.hilgevoord <lucashilgevoord@hotmail.com>
2022-12-06 23:57:37 +0100

    Changed order so wiring is easier

--- testing/segment_display/nano_segment_display/src/main.cpp
+++ testing/segment_display/nano_segment_display/src/main.cpp
@@ -12,7 +12,7 @@ void setup() {
 
     byte numDigits = 4;  
     byte digitPins[] = {2, 3, 4, 5};
-    byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+    byte segmentPins[] = {13, 12, 7, 9, 10, 11, 6, 8};
     bool resistorsOnSegments = 0; 
     sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins, resistorsOnSegments);
     sevseg.setBrightness(90);
