978b5c3d4316ee0290b28ec323751f9c9911b1d5
Lucas Hilgevoord <lucas.hilgevoord@hva.nl>
2022-12-08 17:51:54 +0100

    Added functions

--- testing/segment_display/nano_segment_display/src/main.cpp
+++ testing/segment_display/nano_segment_display/src/main.cpp
@@ -7,32 +7,52 @@ int max_countdown = 5000;
 int number = 0;
 char buffer[5];
 
-void setup() {
-    Serial.begin(9600);
+void setup()
+{
+  Serial.begin(9600);
 
-    byte numDigits = 4;  
-    byte digitPins[] = {2, 3, 4, 5};
-    byte segmentPins[] = {13, 12, 7, 9, 10, 11, 6, 8};
-    bool resistorsOnSegments = 0; 
-    sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins, resistorsOnSegments);
-    sevseg.setBrightness(90);
+  byte numDigits = 4;
+  byte digitPins[] = {2, 3, 4, 5};
+  byte segmentPins[] = {13, 12, 7, 9, 10, 11, 6, 8};
+  bool resistorsOnSegments = 0;
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  sevseg.setBrightness(90);
 }
 
-void loop() {
-    sprintf(buffer, "%04d", number);
-    char numb;
-    for (size_t i = 0; i < 4; i++)
-    {
-      numb = numb + buffer[i];
-    }
-    countdown++;
-
-    if (countdown >= max_countdown) {
-      number++;
-      countdown = 0;
-    }
-
-    sevseg.setChars(buffer);
-    sevseg.refreshDisplay(); // Must run repeatedly
+void update_counter()
+{
+  sprintf(buffer, "%04d", number);
+  sevseg.setChars(buffer);
 }
 
+void add_number(int amount)
+{
+  number += amount;
+  update_counter();
+}
+
+void remove_number(int amount) {
+  add_number(-amount);
+}
+
+void reset() {
+  number = 0;
+  sprintf(buffer, "%04d", number);
+  sevseg.setChars(buffer);
+}
+
+void loop()
+{
+  // countdown++;
+  // if (countdown >= max_countdown)
+  // {
+  //   if (number <= 500) {
+  //     add_number(1);
+  //   } else {
+  //     reset();
+  //   }
+  //   countdown = 0;
+  // }
+
+  sevseg.refreshDisplay(); // Must run repeatedly
+}
