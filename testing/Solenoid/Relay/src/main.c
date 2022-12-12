2fc699b0c00d6378d204816b690163c4bbb5086f
l.hilgevoord <lucashilgevoord@hotmail.com>
2022-12-12 11:11:09 +0100

    Combine relay/segment WIP

--- testing/Solenoid/Relay/src/main.c
+++ testing/Solenoid/Relay/src/main.c
@@ -5,33 +5,33 @@
 #define HIGH 1
 #define LOW 0
 
-int relay_pins[] = {4, 9};
+int relayPins[] = {10, 11};
 int relayLength = 0;
-int loopIndex = 0;
+int relayIndex = 0;
 int up_length = 800;
 
-void app_main()
-{
-    relayLength = sizeof(relay_pins) / sizeof(relay_pins[0]);
-    for (size_t i = 0; i < relayLength; i++)
-    {
-        gpio_set_direction(relay_pins[i], GPIO_MODE_OUTPUT);
-    }
-
-    while (true)
-    {
-        toggle_relay();
-    }
-}
-
-void toggle_relay() {
+void enable_relay() {
     // Turn the relay on
-    gpio_set_level(relay_pins[loopIndex], HIGH);
+    gpio_set_level(relayPins[relayIndex], HIGH);
 
     // Add a delay before turning it off
     vTaskDelay(up_length / portTICK_PERIOD_MS);
 
     // turn the relay off
-    gpio_set_level(relay_pins[loopIndex], LOW);
-    loopIndex = loopIndex < relayLength - 1 ? loopIndex + 1 : 0;
+    gpio_set_level(relayPins[relayIndex], LOW);
+    relayIndex = relayIndex < relayLength - 1 ? relayIndex + 1 : 0;
+}
+
+void app_main()
+{
+    relayLength = sizeof(relayPins) / sizeof(relayPins[0]);
+    for (size_t i = 0; i < relayLength; i++)
+    {
+        gpio_set_direction(relayPins[i], GPIO_MODE_OUTPUT);
+    }
+
+    while (true)
+    {
+        enable_relay();
+    }
 }
--- testing/segment_display/esp_segment_display/src/main.cpp
+++ testing/segment_display/esp_segment_display/src/main.cpp
@@ -7,12 +7,32 @@ char buffer[5];
 byte digitPins[] = {4, 5, 6, 7};
 byte segmentPins[] = {9, 46, 16, 18, 8, 3, 15, 17};
 
-const int buttonPin = 4;
+int relayPins[] = {10, 11};
+int relayCooldown[] = {0, 0};
+int relayUpTime = 1000;
+
+int relayIndex = 1;
+int relayLength = 0;
+
+int buttonPin = 13;
 int buttonState = 0;
 bool isPressed = false;
+int pressDelay = 2000;
+int currentPressDelay = 0;
+
+void enable_relay() {
+    Serial.println("Pressed");
+    if (relayCooldown[relayIndex] <= 0) {
+      Serial.printf("TURN ON: %d \n", relayPins[relayIndex]);
+      digitalWrite(relayPins[relayIndex], HIGH);
+      relayCooldown[relayIndex] = relayUpTime;
+    }
+    relayIndex = relayIndex < relayLength - 1 ? relayIndex + 1 : 0;
+}
 
 void update_counter()
 {
+  enable_relay();
   sprintf(buffer, "%04d", number);
   sevseg.setChars(buffer);
 }
@@ -29,7 +49,7 @@ void add_number(int amount)
   update_counter();
 }
 
-void remove_number(int amount)
+void subtract_number(int amount)
 {
   add_number(-amount);
 }
@@ -44,41 +64,57 @@ void reset()
 void setup()
 {
   Serial.begin(9600);
-  pinMode(buttonPin, INPUT);
-  byte numDigits = 4;
+  pinMode(buttonPin, INPUT_PULLUP);
+  for (size_t i = 0; i < relayLength; i++)
+  {
+    Serial.printf("initializing: %d", i);
+    pinMode(relayPins[i], OUTPUT);
+  }
 
+  byte numDigits = sizeof(digitPins) / sizeof(digitPins[0]);;
+  relayLength = sizeof(relayPins) / sizeof(relayPins[0]);
   bool resistorsOnSegments = 0;
-  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins, resistorsOnSegments);
-  sevseg.setBrightness(90);
+  //sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  //sevseg.setBrightness(90);
+  currentPressDelay = pressDelay;
 
-  reset();
+  //reset();
 }
 
 int incomingByte = 0;
 void loop()
 {
-  //sevseg.refreshDisplay();
   buttonState = digitalRead(buttonPin);
-  Serial.println(buttonState);
-
-  if (buttonState == HIGH)
+  if (buttonState == LOW)
   {
-    // turn on:
-    if (!isPressed)
+    if (!isPressed && currentPressDelay <= 0)
     {
-      //add_number(1);
       isPressed = true;
+      currentPressDelay = pressDelay;
+      //add_number(1);
+      enable_relay();
     }
   }
   else
   {
-    // turn off:
     isPressed = false;
+    if (currentPressDelay > 0){
+      currentPressDelay--;
+    }
   }
-  // printf("loop");
-  // if (Serial.available() > 0) {
-  //   incomingByte = Serial.read();
 
-  //   printf("Serial: %d", incomingByte);
-  // }
+  for (size_t i = 0; i < relayLength; i++)
+  {
+    if (relayCooldown[i] > 0) {
+      relayCooldown[i]--;
+
+      if (relayCooldown[i] <= 0) {
+        Serial.printf("Relay cooldown over %d: %d\n", i, relayCooldown[i]);
+        //digitalWrite(relayPins[relayIndex], LOW);
+        relayCooldown[i] = 0;
+      }
+    }
+  }
+
+  //sevseg.refreshDisplay();
 }
