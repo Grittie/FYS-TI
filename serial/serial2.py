7568137b695574cfedf392a90405da28adc240ca
Tristan de Boer <tristan.de.boer@hva.nl>
2023-01-09 14:53:46 +0000

    Implemented functionality to only show the device addresses of the connected devices These will be used to check if the devices reply to the WAP hello message.

--- serial/serial2.py
+++ serial/serial2.py
@@ -4,12 +4,18 @@ ports = serial.tools.list_ports.comports()
 serialInst = serial.Serial()
 
 portList = []
+portAddress = []
 
 for onePort in ports:
+
     portList.append(str(onePort))
-    print(str(onePort))
+    port = (str(onePort))
+    split_port = port.split(" ", 1)
+    substring = split_port[0]
+    portAddress.append(substring)
 
-
+for address in portAddress:
+    print(address)    
 # arduino = serial.Serial('COM1', 115200, timeout=.1)
 
 # while True:
