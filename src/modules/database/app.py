02df67578cac928aaaa1c4eed30538219052d7b3
Jasper Hoogeboom <jasper.hoogeboom@hva.nl>
2023-01-14 20:37:31 +0100

    Fixed button functions
    when using the buttons, it now navigates to a new page specifically for each button. When the page loads, the test function is called and executed

--- src/modules/database/app.py
+++ src/modules/database/app.py
@@ -7,12 +7,38 @@ app = Flask(__name__)
 devicedataheadings = ("logid", "timestamp", "temperature", "humidity", "knockStatus", "deviceStatus")
 userdataheadings = ("sessionId", "deviceId", "timestamp", "sessionDuration", "score")
 
+@app.route('/testbuttons', methods= ['GET'])
+def testbuttons():
+    # TODO add button test function
+    if request.method == 'GET':
+        print("testbuttons")
 
-@app.route('/')
+    return render_template('testbuttons.html')
+
+
+@app.route('/testmatrix', methods= ['GET'])
+def testmatrix():
+    # TODO add matrix test function
+    if request.method == 'GET':
+        print('testing matrix')
+    return render_template('testmatrix.html')
+
+
+@app.route('/testmascottes', methods= ['GET'])
+def testmascottes():
+    # TODO add mascotte test function
+    if request.method == 'GET':
+        print('testing mascottes')
+    return render_template('testmascottes.html')
+
+@app.route('/db_dashboard', methods= ['GET'])
 def index():  # put application's code here
-
-    devicedata = db_functions.get_devicedata()
-    userdata = db_functions.get_userdata()
+    if request.method == 'GET':
+        devicedata = db_functions.get_devicedata()
+        userdata = db_functions.get_userdata()
+    else:
+        devicedata = db_functions.get_devicedata()
+        userdata = db_functions.get_userdata()
 
     return render_template('index.html', devicedataheadings=devicedataheadings, devicedata=devicedata,
                            userdataheadings=userdataheadings, userdata=userdata)
