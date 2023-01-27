be73549eec22d7ccfcf17e2e628b47aeff4b4092
Tristan de Boer <tristan.de.boer@hva.nl>
2023-01-27 11:54:45 +0100

    Final changes

--- serial/esp32_test/src/main.c
+++ serial/esp32_test/src/main.c
@@ -1,62 +1,52 @@
+/* @author Tristan de Boer
+This code is used to test the functionality of buttons using the communication protocol between the odroid and esp32
+*/
+
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 void app_main() {
-    int btnPins [] = {4,5,6,7,15,16,17};
-    for (int i = 0; i < 6; i++) {
+    int btnPins [] = {4,7,17,3,10};
+    for (int i = 0; i < 4; i++) {
         gpio_set_direction(btnPins[i], GPIO_MODE_INPUT);
     }
     while (true) {
-        if (gpio_get_level(4)) {
+        if (gpio_get_level(btnPins[0])) {
             printf("0x01_129\n");
             // vTaskDelay(100 / portTICK_PERIOD_MS);
             // if (!gpio_get_level(4)) {
             //     printf("0x01_1\n");
             // }
         } 
-        if (gpio_get_level(5)) {
+        if (gpio_get_level(btnPins[1])) {
             printf("0x01_130\n");
             // vTaskDelay(100 / portTICK_PERIOD_MS);
             // if (!gpio_get_level(5)) {
             //     printf("0x01_2\n");
             // }
         } 
-        if (gpio_get_level(6)) {
+        if (gpio_get_level(btnPins[2])) {
             printf("0x01_132\n");
             // vTaskDelay(100 / portTICK_PERIOD_MS);
             // if (!gpio_get_level(6)) {
             //     printf("0x01_4\n");
             // }
         }
-        if (gpio_get_level(7)) {
+        if (gpio_get_level(btnPins[3])) {
             printf("0x01_136\n");
             // vTaskDelay(100 / portTICK_PERIOD_MS);
             // if (!gpio_get_level(7)) {
             //     printf("0x01_8\n");
             // }
         }
-        if (gpio_get_level(15)) {
+        if (gpio_get_level(btnPins[4])) {
             printf("0x01_144\n");
             // vTaskDelay(100 / portTICK_PERIOD_MS);
             // if (!gpio_get_level(15)) {
             //     printf("0x01_16\n");
             // }
         }
-        if (gpio_get_level(16)) {
-            printf("0x01_160\n");
-            // vTaskDelay(100 / portTICK_PERIOD_MS);
-            // if (!gpio_get_level(16)) {
-            //     printf("0x01_32\n");
-            // }
-        }
-        if (gpio_get_level(17)) {
-            printf("0x01_192\n");
-            // vTaskDelay(100 / portTICK_PERIOD_MS);
-            // if (!gpio_get_level(17)) {
-            //     printf("0x01_64\n");
-            // }
-        } 
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
--- serial/serial_odroid.py
+++ serial/serial_odroid.py
@@ -1,121 +1,92 @@
+# @author Tristan de Boer
+# This program is used to test the communication between the button controller via a USB serial connection.
+
 import serial.tools.list_ports
 from threading import Thread
 import time
 
-
+# get all the serial comports
 ports = serial.tools.list_ports.comports()
+# create an instance of the serial method
 serialInst = serial.Serial()
 
 portList = []
 portAddress = []
-btnControllerAddress = ""
 buttonStates = [0,0,0,0,0,0,0,0]
-ledState = [0,0,0,0,0,0,0,0]
+
 
 def identify(address):
-    arduino = serial.Serial(address, 115200, timeout=.1)
+    # create the connection
+    device = serial.Serial(address, 115200, timeout=.1)
     global buttonStates
-    
-    #arduino.write(encoded_led)
-    
     while True:
-        data = arduino.readline()[:-2]
+        data = device.readline()[:-2]
         if data:
-            print(data)
-            #decode = data.decode("ascii")            
-            # device_data = decode.split("_")
-            # device_name = ""
-            # if device_data[0] == "0x01":
-            #     device_name = "button controller"
-            # elif device_data[0] == "0x02":
-            #     device_name = "7 segment controller"
-            # print(device_data)
-            # if device_data[1] == "129":
-            #     buttonStates[0] = 1
-            #     buttonStates[7] = 1
-            #     arduino.write("7".encode())
+            decode = data.decode("ascii")
+            device_data = decode.split("_")
+            device_name = ""
+            if device_data[0] == "0x01":
+                device_name = "button controller"
+            elif device_data[0] == "0x02":
+                device_name = "7 segment controller"
+            print(device_data)
+            if device_data[1] == "129":
+                buttonStates[0] = 1
+                buttonStates[7] = 1
 
-            # if device_data[1] == "1":
-            #     buttonStates[0] = 0
-            #     buttonStates[7] = 0
-            #     arduino.write("0".encode())
+            elif device_data[1] == "1":
+                buttonStates[0] = 0
+                buttonStates[7] = 0
 
-            # if device_data[1] == "130":
-            #     buttonStates[0] = 1
-            #     buttonStates[6] = 1
-            #     arduino.write("6".encode())
-            #     print("130".encode())
+            elif device_data[1] == "130":
+                buttonStates[0] = 1
+                buttonStates[6] = 1
 
-            # if device_data[1] == "2":
-            #     buttonStates[0] = 0
-            #     buttonStates[6] = 0
-            #     arduino.write("0".encode())
+            elif device_data[1] == "2":
+                buttonStates[0] = 0
+                buttonStates[6] = 0
 
-            # if device_data[1] == "132":
-            #     buttonStates[0] = 1
-            #     buttonStates[5] = 1
-            #     arduino.write("132\n".encode("ascii"))
+            elif device_data[1] == "132":
+                buttonStates[0] = 1
+                buttonStates[5] = 1
 
-            # if device_data[1] == "4":
-            #     buttonStates[0] = 0
-            #     buttonStates[5] = 0
-            #     arduino.write("4\n".encode("ascii"))
+            elif device_data[1] == "4":
+                buttonStates[0] = 0
+                buttonStates[5] = 0
 
-            # if device_data[1] == "136":
-            #     buttonStates[0] = 1
-            #     buttonStates[4] = 1
-            #     arduino.write("136\n".encode("ascii"))
+            elif device_data[1] == "136":
+                buttonStates[0] = 1
+                buttonStates[4] = 1
 
-            # if device_data[1] == "8":
-            #     buttonStates[0] = 0
-            #     buttonStates[4] = 0
-            #     arduino.write("8\n".encode("ascii"))
+            elif device_data[1] == "8":
+                buttonStates[0] = 0
+                buttonStates[4] = 0
 
-            # if device_data[1] == "144":
-            #     buttonStates[0] = 1
-            #     buttonStates[3] = 1
-            #     arduino.write("144\n".encode("ascii"))
+            elif device_data[1] == "144":
+                buttonStates[0] = 1
+                buttonStates[3] = 1
 
-            # if device_data[1] == "16":
-            #     buttonStates[0] = 0
-            #     buttonStates[3] = 0
-            #     arduino.write("16\n".encode("ascii"))
+            elif device_data[1] == "16":
+                buttonStates[0] = 0
+                buttonStates[3] = 0
 
-            # if device_data[1] == "160":
-            #     buttonStates[0] = 1
-            #     buttonStates[2] = 1
-            #     arduino.write("160\n".encode("ascii"))
+            elif device_data[1] == "160":
+                buttonStates[0] = 1
+                buttonStates[2] = 1
 
-            # if device_data[1] == "32":
-            #     buttonStates[0] = 0
-            #     buttonStates[2] = 0  
-            #     arduino.write("32\n".encode("ascii"))          
+            elif device_data[1] == "32":
+                buttonStates[0] = 0
+                buttonStates[2] = 0            
 
-            # if device_data[1] == "192":
-            #     buttonStates[0] = 1
-            #     buttonStates[1] = 1
-            #     arduino.write("192\n".encode("ascii"))
+            elif device_data[1] == "192":
+                buttonStates[0] = 1
+                buttonStates[1] = 1
 
-            # if device_data[1] == "64":
-            #     buttonStates[0] = 0
-            #     buttonStates[1] = 0
-            #     arduino.write("64\n".encode("ascii"))
-        
-        while True:
-            if (ledState[0] == 1) and (ledState[7] == 1):
-                arduino.write("129".encode("ascii"))
-                print("Set led 7 on")
-            if (ledState[0] == 0) and (ledState[7] == 0):
-                arduino.write("1".encode("ascii"))
-                print("Set led 7 off")
-            if (ledState[0] == 1) and (ledState[6] == 1):
-                arduino.write("130".encode("ascii"))
-                print("Set LED 6 on")
-            if (ledState[0] == 0) and (ledState[6] == 0):
-                arduino.write("2".encode("ascii"))
-                print("Set LED 6 off")
-            time.sleep(1)
+            elif device_data[1] == "64":
+                buttonStates[0] = 0
+                buttonStates[1] = 0
 
+# create and array with the connected serial devices
 for onePort in ports:
     portList.append(str(onePort))
     port = (str(onePort))
@@ -123,10 +94,11 @@ for onePort in ports:
     substring = split_port[0]
     portAddress.append(substring)
 
+# method to keep checking for changes
 def checkforbuttonpress():
     while True:
-        time.sleep(0.05)
-        #print(buttonStates)
+        time.sleep(1)
+        print(buttonStates)
         if (buttonStates[0] == 1) and (buttonStates[7] == 1):
             print("button 7 is pressed")
         elif (buttonStates[0] == 1) and (buttonStates[6] == 1):
@@ -142,56 +114,11 @@ def checkforbuttonpress():
         elif (buttonStates[0] == 1) and (buttonStates[1] == 1):
             print("button 1 is pressed")
 
-
-def cycleLed():
-    while True:
-        ledState[0] = 1
-        ledState[1] = 1
-        time.sleep(1)
-        ledState[0] = 0
-        ledState[1] = 0
-        time.sleep(1)
-        ledState[0] = 1
-        ledState[2] = 1
-        time.sleep(1)
-        ledState[0] = 0
-        ledState[2] = 0
-        time.sleep(1)
-        ledState[0] = 1
-        ledState[3] = 1
-        time.sleep(1)
-        ledState[0] = 0
-        ledState[3] = 0
-        time.sleep(1)
-        ledState[0] = 1
-        ledState[4] = 1
-        time.sleep(1)
-        ledState[0] = 0
-        ledState[4] = 0
-        time.sleep(1)
-        ledState[0] = 1
-        ledState[5] = 1
-        time.sleep(1)
-        ledState[0] = 0
-        ledState[5] = 0
-        time.sleep(1)
-        ledState[0] = 1
-        ledState[6] = 1
-        time.sleep(1)
-        ledState[0] = 0
-        ledState[6] = 0
-        time.sleep(1)
-        ledState[0] = 1
-        ledState[7] = 1
-        time.sleep(1)
-        ledState[0] = 0
-        ledState[7] = 0
-        
+# create a thread for each connected serial device
 for address in portAddress:
     address = Thread(target=identify, args=[address])
     address.start()
 
+# create a thread for the buttonpresscheck
 threadcheckingforbuttonpress = Thread(target=checkforbuttonpress)
 threadcheckingforbuttonpress.start()
-threadupdatingleds = Thread(target=cycleLed)
-threadupdatingleds.start()
